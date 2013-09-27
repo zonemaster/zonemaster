99f6380ccc24b8d1a0620dbbff89a8ebc2227378
Calle Dybedahl <calle@init.se>
2013-09-27 12:11:36 +0200

    Fix bug that led to multiple caching of nameservers.

--- Giraffa/lib/Giraffa/Nameserver.pm
+++ Giraffa/lib/Giraffa/Nameserver.pm
@@ -39,16 +39,16 @@ around 'new' => sub {
     my $self = shift;
 
     my $obj = $self->$orig( @_ );
-    my $name = $obj->name;
+    my $name = uc('' . $obj->name);
     $name = '$$$NONAME' unless $name;
 
-    if (not exists $object_cache{ '' . $name }{ $obj->address->ip }) {
-        Giraffa->logger->add(NS_CREATED => {name => "$name", ip => $obj->address->ip});
-        $object_cache{ '' . $name }{ $obj->address->ip } = $obj;
+    if (not exists $object_cache{ $name }{ $obj->address->ip }) {
+        Giraffa->logger->add(NS_CREATED => {name => $name, ip => $obj->address->ip});
+        $object_cache{ $name }{ $obj->address->ip } = $obj;
     }
 
-    Giraffa->logger->add(NS_FETCHED => {name => "$name", ip => $obj->address->ip});
-    return $object_cache{ '' . $name }{ $obj->address->ip };
+    Giraffa->logger->add(NS_FETCHED => {name => $name, ip => $obj->address->ip});
+    return $object_cache{ $name }{ $obj->address->ip };
 };
 
 sub _build_dns {
