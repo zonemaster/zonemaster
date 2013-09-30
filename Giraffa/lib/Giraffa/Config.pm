97bba84f129f3d83005da1e698661028090d98c1
Calle Dybedahl <calle@init.se>
2013-09-30 10:58:52 +0200

    Config loads policy (which is at the moment empty).

--- Giraffa/lib/Giraffa/Config.pm
+++ Giraffa/lib/Giraffa/Config.pm
@@ -2,16 +2,25 @@ package Giraffa::Config v0.0.1;
 
 use 5.14.2;
 use Moose;
-use JSON::PP;
+use JSON;
+use File::ShareDir qw[dist_file];
+use File::Slurp;
 
 use Giraffa;
 
 our $config = decode_json( join( '', <DATA> ) );
+our $policy = decode_json read_file dist_file('Giraffa', 'policy.json');
 
 sub get {
     my ( $class ) = @_;
 
     return $config;
+}
+
+sub policy {
+    my ( $class ) = @_;
+
+    return $policy;
 }
 
 1;
@@ -32,6 +41,10 @@ Giraffa::Config - configuration access module for Giraffa
 
 Returns a reference to a hash with configuration values. As of this writing, this is simply seeded from static values hardcoded into the module.
 This is intended to change.
+
+=item policy()
+
+Returns a reference to the current policy data. The format of that data is yet to be decided on.
 
 =back
 
--- Giraffa/t/config.t
+++ Giraffa/t/config.t
@@ -9,4 +9,7 @@ isa_ok( $ref, 'HASH' );
 is( $ref->{resolver}{defaults}{retry},   1, 'retry exists and has expected value' );
 is( config->{resolver}{defaults}{retry}, 1, 'access other way works too' );
 
+my $policy = Giraffa::Config->policy;
+isa_ok($policy, 'HASH', 'policy got loaded and');
+
 done_testing;
