03bb44c7b3ad14625047d075cf033855b381fd3f
Calle Dybedahl <github@cyberpomo.com>
2014-01-21 11:47:15 +0100

    Implement syntax tests according to spec.

--- Giraffa/lib/Giraffa/Test/Syntax.pm
+++ Giraffa/lib/Giraffa/Test/Syntax.pm
@@ -6,6 +6,11 @@ use warnings;
 
 use Giraffa;
 use Giraffa::Util;
+use Giraffa::Recursor;
+
+use List::MoreUtils;
+use RFC::RFC822::Address qw[valid];
+use Time::Local;
 
 ###
 ### Entry points
@@ -16,13 +21,23 @@ sub all {
     my @results;
 
     push @results, $class->name_syntax( $zone->name );
-    foreach my $name ( keys %{ { map { $_->name => 1 } @{ $zone->ns } } } ) {
+    foreach my $name ( keys %{ { map { $_->name => 1 } (@{ $zone->ns }, @{ $zone->glue }) } } ) {
         push @results, $class->name_syntax( $name );
     }
 
     my $p = $zone->query_one( $zone->name, 'SOA' );
     my ( $soa ) = $p->get_records( 'SOA' );
-    push @results, $class->name_syntax( $soa->mname );
+    push @results, $class->name_syntax( $soa->mname );  # syntax07
+    push @results, $class->rname_syntax( $soa->rname ); # syntax05 and syntax06
+    push @results, $class->soa_date( $soa->serial );    # syntax09
+
+    # syntax08
+    my $rname = $soa->rname;
+    $rname =~ s/[^\\]\.(.*)+$/$1/; # Keep everything after the first unescaped dot
+    $p = Giraffa::Recursor->recurse( $rname, 'MX');
+    foreach my $mx (grep {$_->type eq 'MX'} $p->answer) {
+        push @results, $class->name_syntax( $mx->exchange );
+    }
 
     return @results;
 }
@@ -37,14 +52,27 @@ sub metadata {
     return {
         name_syntax => [
             qw(
-              CHECKING_NAME
-              NUMERIC_TOP
-              NON_NUMERIC_TOP
-              DASH_END
-              NO_DASH_END
-              ALPHANUMERIC_START
-              NON_ALPHANUMERIC_START
+                CHECKING_NAME
+                ONLY_ALLOWED_CHARS
+                NON_ALLOWED_CHARS
+                INITIAL_HYPHEN
+                TERMINAL_HYPHEN
+                DISCOURAGED_DOUBLE_DASH
+                NUMERIC_TLD
+                NAME_TOO_LONG
+                LABEL_TOO_LONG
               )
+        ],
+        rname_syntax => [
+            qw(
+                RNAME_MISUSED_AT_SIGN
+                RNAME_RFC822_INVALID
+            )
+        ],
+        soa_date => [
+            qw(
+                
+            )
         ]
     };
 }
@@ -67,36 +95,93 @@ sub name_syntax {
 
     info( CHECKING_NAME => { name => "$name" } );
 
-    # Checking total and label lengths is pointless, since if they were too long we
-    # could not recieve them in a DNS packet.
-
-    # RFC 3696: Top-level may not be all-numeric
-    if ( $name->labels->[-1] =~ /^\d+$/ ) {
-        push @results, info( NUMERIC_TOP => { name => "$name" } );
+    # syntax01
+    if (List::MoreUtils::all {m/^[-A-Za-z0-9]+$/} @{$name->labels}) {
+        push @results, info( ONLY_ALLOWED_CHARS => { name => "$name"});
     }
     else {
-        push @results, info( NON_NUMERIC_TOP => { name => "$name" } );
+        push @results, info( NON_ALLOWED_CHARS => { name => "$name"});
     }
 
-    # RFC 952: Labels may not end with a dash
-    my $oops = 0;
-    foreach my $l ( @{ $name->labels } ) {
-        if ( $l =~ /\-$/ ) {
-            push @results, info( DASH_END => { name => "$name" } );
-            $oops = 1;
+    # syntax02
+    foreach my $label (@{ $name->labels }) {
+        if ($label =~ /^-/) {
+            push @results, info( INITIAL_HYPHEN => { label => $label, name => "$name"});
+        }
+        if ($label =~ /-$/) {
+            push @results, info( TERMINAL_HYPHEN => { label => $label, name => "$name"});
         }
     }
-    push @results, info( NO_DASH_END => { name => "$name" } ) if not $oops;
 
-    # RFC 952 and RFC 1123: First label must start with alphanumeric
-    if ( $name->labels->[0] =~ /^[a-z0-9]/i ) {
-        push @results, info( ALPHANUMERIC_START => { name => "$name" } );
+    # syntax03
+    foreach my $label (@{ $name->labels }) {
+        if (($label =~ /^..--/) and not ($label =~ /^xn/) ) {
+            push @results, info( DISCOURAGED_DOUBLE_DASH => { label => $label, name => "$name"});
+        }
     }
-    else {
-        push @results, info( NON_ALPHANUMERIC_START => { name => "$name" } );
+
+    # syntax04, numeric TLD subtest
+    my $tld = @{$name->labels}[-1];
+    if ($tld =~ /^[0-9]+$/) {
+        push @results, info( NUMERIC_TLD => { name => "$name"});
+    }
+
+    # syntax04, size subtests, can only fail for manually added names
+    if (length("$name") > 254) { # $name plus terminal dot > 255
+        push @results, info( NAME_TOO_LONG => { name => "$name", length => length("$name")});
+    }
+    foreach my $label (@{$name->labels}) {
+        if (length($label) > 63) {
+            push @results, info( LABEL_TOO_LONG => { name => "$name", label => $label, length => length($label)});
+        }
     }
 
     return @results;
+}
+
+sub rname_syntax {
+    my ($self, $rname ) = @_;
+    my @results;
+
+    info( CHECKING_RNAME => { rname => $rname });
+
+    # syntax05
+    my $copy = $rname;
+    $copy =~ s/\\././g;
+    if (index($copy,'@')!=-1) {
+        push @results, info( RNAME_MISUSED_AT_SIGN => {rname => $rname});
+    }
+
+    # syntax06
+    $rname =~ s/([^\\])\./$1@/; # Replace first non-escaped dot with an at-sign
+    $rname =~ s/\\././g;        # Un-escape dots
+    $rname =~ s/\.$//;          # Validator does not like final dots
+    if (not valid($rname)) {
+        warn "Invalid: $rname";
+        push @results, info( RNAME_RFC822_INVALID => { rname => $rname } );
+    }
+
+    return @results;
+}
+
+sub soa_date {
+    my ( $self, $serial ) = @_;
+
+    if (length($serial) != 10) { # Wrong length, can't be date+counter
+        return info( SERIAL_NOT_DATE => { serial => $serial, why => 'wrong length' });
+    }
+
+    my ($year, $month, $day) = $serial =~ m|^([0-9]{4})([0-9]{2})([0-9]{2})|;
+    if (not (defined($year) and defined($month) and defined($day))) {
+        return info( SERIAL_NOT_DATE => { serial => $serial, why => 'wrong format' });
+    }
+
+    my $t = eval {timegm(0,0,0,$day,$month-1,$year)};
+    if (not defined($t)) {
+        return info( SERIAL_NOT_DATE => { serial => $serial, why => 'invalid date' });
+    }
+
+    return;
 }
 
 1;
