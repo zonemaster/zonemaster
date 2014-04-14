d7c0bd7d4e2599b17fb22be933cb28ce409e732d
Calle Dybedahl <calle@init.se>
2014-04-14 11:13:06 +0200

    Add JSON output.

--- Giraffa-CLI/lib/Giraffa/CLI.pm
+++ Giraffa-CLI/lib/Giraffa/CLI.pm
@@ -11,8 +11,10 @@ with 'MooseX::Getopt';
 use Giraffa;
 use Giraffa::Logger::Entry;
 use Giraffa::Translator;
+use JSON::XS;
 
 our %numeric = Giraffa::Logger::Entry->levels;
+my $json = JSON::XS->new;
 
 has 'level' => (
     is            => 'ro',
@@ -22,10 +24,11 @@ has 'level' => (
     documentation => 'The minimum severity level to display'
 );
 has 'lang' =>
-  ( is => 'ro', isa => 'Str', required => 0, default => 'tech', documentation => 'The language to show messages in' );
+  ( is => 'ro', isa => 'Str', required => 0, default => 'tech', documentation => 'The language to show messages in. Can be tech, raw, json or any installed translation.' );
 
 sub run {
     my ( $self ) = @_;
+    my @accumulator;
 
     my ( $domain ) = @{ $self->extra_argv };
     if ( not $domain ) {
@@ -33,8 +36,8 @@ sub run {
     }
 
     my $translator;
-    $translator = Giraffa::Translator->new( { lang => $self->lang } ) unless $self->lang eq 'raw';
-    eval { $translator->data }; # Provoke lazy loading of translation data
+    $translator = Giraffa::Translator->new( { lang => $self->lang } ) unless ($self->lang eq 'raw' or $self->lang eq 'json');
+    eval { $translator->data } if $translator; # Provoke lazy loading of translation data
     if ( $@ ) {
         if ( $@ =~ /Cannot read translation file/ ) {
             say "Cannot find a translation for language " . $self->lang . ".";
@@ -52,14 +55,27 @@ sub run {
             return if $numeric{ uc $entry->level } < $numeric{ uc $self->level };
 
             if ( $translator ) {
-                say $translator->translate( $entry );
+                say $translator->translate_tag( $entry );
+            }
+            elsif ($self->lang eq 'json') {
+                push @accumulator, {
+                    timestamp => $entry->timestamp,
+                    module => $entry->module,
+                    tag => $entry->tag,
+                    args => $entry->args,
+                };
             }
             else {
                 say "$entry";
             }
         }
     );
+
     Giraffa->test_zone( $domain );
+
+    if ($self->lang eq 'json') {
+        say $json->encode(\@accumulator);
+    }
 
     return;
 } ## end sub run
