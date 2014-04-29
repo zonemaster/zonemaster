2cdc1646c3665d963e167a6361609771d13d1556
Calle Dybedahl <calle@init.se>
2014-04-29 10:35:06 +0200

    Add ability to load policy and configuration information from specified files.

--- Giraffa/lib/Giraffa/Config.pm
+++ Giraffa/lib/Giraffa/Config.pm
@@ -5,10 +5,34 @@ use Moose;
 use JSON;
 use File::ShareDir qw[dist_file];
 use File::Slurp;
+use Hash::Merge;
 
 use Giraffa;
 
-our $config = decode_json( join( '', <DATA> ) );
+my $merger = Hash::Merge->new;
+$merger->specify_behavior(
+    {
+        'SCALAR' => {
+            'SCALAR' => sub { $_[1] },
+            'ARRAY'  => sub { [ $_[0], @{ $_[1] } ] },
+            'HASH'   => sub { $_[1] },
+        },
+        'ARRAY' => {
+            'SCALAR' => sub { $_[1] },
+            'ARRAY'  => sub { [ @{ $_[1] } ] },
+            'HASH'   => sub { $_[1] },
+        },
+        'HASH' => {
+            'SCALAR' => sub { $_[1] },
+            'ARRAY'  => sub { [ values %{ $_[0] }, @{ $_[1] } ] },
+            'HASH'   => sub { Hash::Merge::_merge_hashes( $_[0], $_[1] ) },
+        },
+    }
+);
+
+our $config;
+_load_base_config();
+
 our $policy = decode_json read_file dist_file( 'Giraffa', 'policy.json' );
 
 sub get {
@@ -21,6 +45,31 @@ sub policy {
     my ( $class ) = @_;
 
     return $policy;
+}
+
+sub _load_base_config {
+    my $internal = decode_json( join( '', <DATA> ) );
+    my $default = eval { decode_json read_file dist_file( 'Giraffa', 'config.json') };
+
+    $internal = $merger->merge($internal, $default) if $default;
+
+    $config = $internal;
+}
+
+sub load_config_file {
+    my ( $class, $filename ) = @_;
+    my $new = decode_json read_file $filename;
+    $config = $merger->merge($config,$new) if $new;
+
+    return;
+}
+
+sub load_policy_file {
+    my ( $class, $filename ) = @_;
+    my $new = decode_json read_file $filename;
+    $policy = $merger->merge($policy, $new) if $new;
+
+    return;
 }
 
 1;
@@ -45,6 +94,14 @@ This is intended to change.
 =item policy()
 
 Returns a reference to the current policy data. The format of that data is yet to be decided on.
+
+=item load_policy_file($filename)
+
+Load policy information from the given file and merge it into the pre-loaded policy. Information from the loaded file overrides the pre-loaded information when the same keys exist in both places.
+
+=item load_config_file($filename)
+
+Load configuration information from the given file and merge it into the pre-loaded config. Information from the loaded file overrides the pre-loaded information when the same keys exist in both places.
 
 =back
 
