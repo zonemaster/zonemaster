4bdee0b0186a2dce604ec8f7010b39242b632af8
vlevigneron <vincent.levigneron@nic.fr>
2014-05-07 00:16:15 +0200

    Cosmetic changes.

--- Zonemaster/lib/Zonemaster/Test/Basic.pm
+++ Zonemaster/lib/Zonemaster/Test/Basic.pm
@@ -15,16 +15,16 @@ sub all {
     my ( $class, $zone ) = @_;
     my @results;
 
-    push @results, $class->basic1( $zone );
+    push @results, $class->basic01( $zone );
 
     # Perform BASIC2 if BASIC1 passed
-    if ( grep { $_->tag eq 'HAS_GLUE' } @results ) {
-        push @results, $class->basic2( $zone );
+    if ( grep { $_->tag eq q{HAS_GLUE} } @results ) {
+        push @results, $class->basic02( $zone );
     }
 
     # Perform BASIC3 if BASIC2 failed
-    if ( not grep { $_->tag eq 'HAS_NAMESERVERS' } @results ) {
-        push @results, $class->basic3( $zone );
+    if ( not grep { $_->tag eq q{HAS_NAMESERVERS} } @results ) {
+        push @results, $class->basic03( $zone );
     }
 
     return @results;
@@ -50,9 +50,23 @@ sub metadata {
     my ( $class ) = @_;
 
     return {
-        basic1 => [qw(HAS_GLUE NO_GLUE NO_DOMAIN NO_PARENT_RESPONSE )],
-        basic2 => [qw(NS_FAILED NS_NO_RESPONSE HAS_NAMESERVERS)],
-        basic3 => [qw(HAS_A_RECORDS)],
+        basic01 => [
+            qw(
+              HAS_GLUE NO_GLUE
+              NO_DOMAIN NO_PARENT_RESPONSE
+              )
+        ],
+        basic02 => [
+            qw(
+              NS_FAILED NS_NO_RESPONSE
+              HAS_NAMESERVERS
+              )
+        ],
+        basic03 => [
+            qw(
+              HAS_A_RECORDS
+              )
+        ],
     };
 }
 
@@ -64,28 +78,38 @@ sub version {
 ### Tests
 ###
 
-sub basic1 {
+sub basic01 {
     my ( $class, $zone ) = @_;
     my @results;
 
     my $parent = $zone->parent;
-    my $p = $parent->query_one( $zone->name, 'NS' );
+    my $p = $parent->query_one( $zone->name, q{NS} );
 
     if ( not $p ) {
-        push @results, info( NO_PARENT_RESPONSE => { parent => $parent->name->string } );
+        push @results,
+          info(
+            NO_PARENT_RESPONSE => {
+                parent => $parent->name->string,
+            }
+          );
         return @results;
     }
 
-    if ( $p->rcode eq 'NXDOMAIN' ) {
-        push @results, info( NO_DOMAIN => { parent => $parent->name->string } );
+    if ( $p->rcode eq q{NXDOMAIN} ) {
+        push @results,
+          info(
+            NO_DOMAIN => {
+                parent => $parent->name->string,
+            }
+          );
     }
     else {
-        if ( $p->has_rrs_of_type_for_name( 'ns', $zone->name ) ) {
+        if ( $p->has_rrs_of_type_for_name( q{NS}, $zone->name ) ) {
             push @results,
               info(
                 HAS_GLUE => {
                     parent => $parent->name->string,
-                    ns     => join( ',', sort map { $_->nsdname } $p->get_records_for_name( 'ns', $zone->name ) )
+                    ns     => join( q{,}, sort map { $_->nsdname } $p->get_records_for_name( q{NS}, $zone->name ) ),
                 }
               );
         }
@@ -94,58 +118,75 @@ sub basic1 {
               info(
                 NO_GLUE => {
                     parent => $parent->name->string,
-                    rcode  => $p->rcode
+                    rcode  => $p->rcode,
                 }
               );
         }
     } ## end else [ if ( $p->rcode eq 'NXDOMAIN')]
 
     return @results;
-} ## end sub basic1
+} ## end sub basic01
 
-sub basic2 {
+sub basic02 {
     my ( $class, $zone ) = @_;
     my @results;
 
     foreach my $ns ( @{ $zone->glue } ) {
-        my $p = $ns->query( $zone->name, 'NS' );
+        my $p = $ns->query( $zone->name, q{NS} );
         if ( $p ) {
-            if ( $p->has_rrs_of_type_for_name( 'ns', $zone->name ) ) {
+            if ( $p->has_rrs_of_type_for_name( q{NS}, $zone->name ) ) {
                 push @results,
                   info(
                     HAS_NAMESERVERS => {
-                        ns     => join( ',', sort map { $_->nsdname } $p->get_records_for_name( 'ns', $zone->name ) ),
-                        source => $ns->string
+                        ns     => join( q{,}, sort map { $_->nsdname } $p->get_records_for_name( q{NS}, $zone->name ) ),
+                        source => $ns->string,
                     }
                   );
             }
             else {
-                push @results, info( NS_FAILED => { source => $ns->string, rcode => $p->rcode } );
+                push @results,
+                  info(
+                    NS_FAILED => {
+                        source => $ns->string,
+                        rcode  => $p->rcode,
+                    }
+                  );
             }
         }
         else {
-            push @results, info( NS_NO_RESPONSE => { source => $ns->string } );
+            push @results,
+              info(
+                NS_NO_RESPONSE => {
+                    source => $ns->string,
+                }
+              );
         }
     } ## end foreach my $ns ( @{ $zone->...})
 
     return @results;
-} ## end sub basic2
+} ## end sub basic02
 
-sub basic3 {
+sub basic03 {
     my ( $class, $zone ) = @_;
     my @results;
 
-    my $name = 'www.' . $zone->name;
+    my $name = q{www.} . $zone->name;
     foreach my $ns ( @{ $zone->glue } ) {
-        my $p = $ns->query( $name, 'A' );
+        my $p = $ns->query( $name, q{A} );
         next if not $p;
-        if ( $p->has_rrs_of_type_for_name( 'a', $name ) ) {
-            push @results, info( HAS_A_RECORDS => { source => $ns->string, name => $name } );
+        if ( $p->has_rrs_of_type_for_name( q{A}, $name ) ) {
+            push @results,
+              info(
+                HAS_A_RECORDS => {
+                    source => $ns->string,
+                    name   => $name,
+                }
+              );
         }
     }
 
     return @results;
-}
+} ## end sub basic03
 
 1;
 
@@ -163,7 +204,7 @@ Zonemaster::Test::Basic - module implementing test for very basic domain functio
 
 =item all($zone)
 
-Runs between one and three tests, depending on the zone. If L<basic1> passes, L<basic2> is run. If L<basic2> fails, L<basic3> is run.
+Runs between one and three tests, depending on the zone. If L<basic01> passes, L<basic02> is run. If L<basic02> fails, L<basic03> is run.
 
 =item metadata()
 
@@ -184,20 +225,20 @@ Looks at the provided log entries and returns true if they indicate that further
 
 =over
 
-=item basic1
+=item basic01
 
 Checks that we can find a parent zone for the zone we're testing. If we can't, no further testing is done.
 
-=item basic2
+=item basic02
 
 Checks that the nameservers for the parent zone returns NS records for the tested zone, and that at least one of the nameservers thus pointed out
 responds sensibly to an NS query for the tested zone.
 
-=item basic3
+=item basic03
 
 Checks if at least one of the nameservers pointed out by the parent zone gives a useful response when sent an A query for the C<www> label in the
 tested zone (that is, if we're testing C<example.org> this test will as for A records for C<www.example.org>). This test is only run if the
-L<basic2> test has I<failed>.
+L<basic02> test has I<failed>.
 
 =back
 
