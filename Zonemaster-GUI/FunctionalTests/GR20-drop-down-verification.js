624501b13a703562b7a71067d3f93a3772d073c3
Sandoche Balakrichenan <sandoche.balakrichenan@afnic.fr>
2014-09-26 12:45:19 +0200

    functional tests for GR19 and GR20 added

---
+++ Zonemaster-GUI/FunctionalTests/GR20-drop-down-verification.js
@@ -0,0 +1,39 @@
+var main_url = 'http://zonemaster.rd.nic.fr:5080/';
+
+var x = require('casper').selectXPath;
+casper.test.begin('Zonemaster test GR20 - [Capable to select one of the drop down menu in the advanced option]', 4, function suite(test) {
+
+	casper.start();
+
+	casper.thenOpen(main_url, function(response) {
+		test.assert(response != undefined && response.status == 200 , 'Page loaded');
+	});
+
+	// Click advanced options
+	casper.then(
+		function() {
+			this.click(x('//label[contains(., "Advanced options")]//input[@type="checkbox"]'));
+		}
+	);
+
+	//
+	casper.then(function() {
+		test.assertExists(".ng-valid.ng-dirty");
+		this.click(".ng-valid.ng-dirty");
+		test.assert(
+                        this.click(x('//option[@value="test_profile_1" and contains(., "Test profile 1")]')),
+                        'Able to select Test profile 1'
+                );
+		this.click(".ng-valid.ng-dirty");
+                test.assert(
+                        this.click(x('//option[@value="test_profile_2" and contains(., "Test profile 2")]')),
+                        'Able to select Test profile 2'
+                );
+               
+	});
+
+	casper.run(function() {
+		test.done();
+	});
+	
+});
