ddea912b475eceac42367c1f5bd08a5b5fba9ffd
Calle Dybedahl <calle@init.se>
2014-10-02 11:12:25 +0200

    Load zone and send very basic responses.

--- Zonemaster-Mock/lib/Zonemaster/Mock/Nameserver.pm
+++ Zonemaster-Mock/lib/Zonemaster/Mock/Nameserver.pm
@@ -2,23 +2,36 @@ package Zonemaster::Mock::Nameserver;
 
 use 5.14.2;
 use Moose;
+use Moose::Util::TypeConstraints;
+use Time::HiRes qw[time];
 
-use Zonemaster::Util;
-use Zonemaster::Net::IP;
+use Zonemaster;
 
-subtype 'Zonemaster::Net::IP', as 'Object', where { $_->isa( 'Net::IP' ) };
-coerce 'Zonemaster::Net::IP', from 'Str', via { Net::IP->new( $_ ) };
-
-has 'name'    => ( is => 'ro', isa => 'Zonemaster::DNSName', coerce => 1, required => 0 );
+has 'name'    => ( is => 'ro', isa => 'Zonemaster::DNSName', coerce => 1, required => 1 );
 has 'address' => ( is => 'ro', isa => 'Zonemaster::Net::IP', coerce => 1, required => 1 );
 
-has 'db' => ( is => 'ro', isa => 'HashRef', default => sub {{}});
+has 'db'       => ( is => 'ro', isa => 'HashRef', default => sub { {} } );
+has 'zones'    => ( is => 'ro', isa => 'HashRef', default => sub { {} } );
+has 'subzones' => ( is => 'ro', isa => 'HashRef', default => sub { {} } );
+
+our $id = 1;
+
+sub dname { Zonemaster::DNSName->new(@_)}
+sub next_id { return $id++}
 
 sub load {
     my ( $self, @rrs ) = @_;
 
-    foreach my $rr (@rrs) {
-        push @{$self->db->{name($rr->name)}}{$rr->class}{$rr->type}, $rr;
+    my $soa      = $rrs[0];
+    my $zonename = dname( $soa->name );
+
+    $self->zones->{$zonename} = $soa;
+    foreach my $rr ( @rrs ) {
+        my $n = dname( $rr->name );
+        push @{ $self->db->{$n}{ $rr->class }{ $rr->type } }, $rr;
+        if ( $rr->type eq 'NS' and $n ne $zonename ) {
+            $self->subzones->{$n} = $n;
+        }
     }
 
     return;
@@ -27,25 +40,17 @@ sub load {
 sub query {
     my ( $self, $name, $type, $href ) = @_;
     my $class = $href->{class} // 'IN';
-    my $p = Net::LDNS::Packet->new("$name",$type,$class);
+    my $p = Net::LDNS::Packet->new( "$name", $type, $class );
+    $p->answerfrom($self->address->short);
+    $p->id(next_id());
+    $p->timestamp(time());
 
-    $name = name($name);
+    $name = dname( $name );
 
-    my $rrset = $self->db->{$name}{uc($class)}{uc($type)};
+    my $rrset = $self->db->{$name}{ uc( $class ) }{ uc( $type ) };
 
-    if (not $rrset or @$rrset == 0) {
-        if ($self->db->{$name}) {
-            return $self->empty_response($p);
-        }
-        elsif (my $super = $self->known_superdomain($name)) {
-            return $self->referral($super, $p);
-        }
-        else {
-            return $self->nxdomain($p);
-        }
-    }
-    else {
-        return $self->answer($rrset, $p);
+    if ($rrset and @$rrset > 0) { # Found data, so answer with it
+        return $self->answer( $rrset, $p );
     }
 }
 
@@ -70,19 +75,12 @@ sub nxdomain {
 sub answer {
     my ( $self, $rrset, $p ) = @_;
 
-    ...;
-}
-
-sub known_superdomain {
-    my ( $self, $name ) = @_;
-
-    while ($name = $name->next_higher) {
-        if ($self->db->{$name}{'IN'}{'NS'}) {
-            return $name;
-        }
+    foreach my $rr (@$rrset) {
+        $p->unique_push('answer', $rr);
     }
 
-    return;
+    $p->aa(1);
+    return $p;
 }
 
 1;
