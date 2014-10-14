44ef82cbd79b0c11eb56d4886cf4ab9881a99d5e
Calle Dybedahl <calle@init.se>
2014-10-14 08:52:23 +0200

    Move to a separate repository (https://github.com/cdybedahl/zonemaster-distributed), since it's not included in the requirements and specifications for Zonemaster.

--- Zonemaster-Distributed/dispatch/views/inprogress/map.js
+++
@@ -1,6 +0,0 @@
-function(doc){
-    if (doc.results[0]["start_time"] && !doc.results[0]["end_time"]) {
-        var r = doc.results[0];
-        emit([r.nodename, r.scanner_pid, doc.name], 1);
-    }
-}
--- Zonemaster-Distributed/dispatch/views/unclaimed/map.js
+++
@@ -1,5 +0,0 @@
-function map(doc) {
-    if (doc.request && !doc.results) {
-        emit(doc.name, null);
-    }
-}
