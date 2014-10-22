f6be5e755d72ee4ebba02b441e6467bc9d7e2457
vlevigneron <vincent.levigneron@nic.fr>
2014-10-23 00:21:52 +0200

    - Zonemaster::TestMethods PBP

--- Zonemaster/lib/Zonemaster/TestMethods.pm
+++ Zonemaster/lib/Zonemaster/TestMethods.pm
@@ -13,13 +13,13 @@ sub method1 {
     my ( $class, $zone ) = @_;
 
     return $zone->parent;
-} ## end sub method1
+}
 
 sub method2 {
     my ( $class, $zone ) = @_;
 
     return $zone->glue_names;
-} ## end sub method2
+}
 
 sub method3 {
     my ( $class, $zone ) = @_;
@@ -27,26 +27,26 @@ sub method3 {
     my @child_nsnames;
     my @nsnames;
     my $ns_aref = $zone->query_all( $zone->name, q{NS} );
-    foreach my $p ( @$ns_aref ) {
+    foreach my $p ( @{$ns_aref} ) {
         next if not $p;
         push @nsnames, $p->get_records_for_name( q{NS}, $zone->name );
     }
-    @child_nsnames = uniq map { name(lc($_->nsdname)) } @nsnames;
+    @child_nsnames = uniq map { name( lc( $_->nsdname ) ) } @nsnames;
 
-    return [ @child_nsnames ];
-} ## end sub method3
+    return [@child_nsnames];
+}
 
 sub method4 {
     my ( $class, $zone ) = @_;
 
     return $zone->glue;
-} ## end sub method4
+}
 
 sub method5 {
     my ( $class, $zone ) = @_;
 
     return $zone->ns;
-} ## end sub method5
+}
 
 =head1 NAME
 
