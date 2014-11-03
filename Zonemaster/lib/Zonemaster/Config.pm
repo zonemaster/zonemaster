2f8419fd9e9098041ba06e9ba2058b52e7bb1e07
Calle Dybedahl <calle@init.se>
2014-11-03 08:57:38 +0100

    Fix thinko in Config.

--- Zonemaster/lib/Zonemaster/Config.pm
+++ Zonemaster/lib/Zonemaster/Config.pm
@@ -141,7 +141,7 @@ sub load_policy_file {
     my $new = decode_json read_file $filename;
     if ($new) {
         $policy = $merger->merge( $policy, $new );
-        push @{$self->pfiles}, $filename;
+        push @{$self->pfiles}, $filename if (ref($self) and $self->isa(__PACKAGE__));
     }
 
     return !!$new;
