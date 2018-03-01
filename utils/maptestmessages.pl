8187ce1cfa792a78970ae7b97c89b32376c06f36
Mattias Päivärinta <mattias.paivarinta@iis.se>
2018-03-01 09:56:53 +0100

    maptestmessages: Whitespace

--- utils/maptestmessages.pl
+++ utils/maptestmessages.pl
@@ -17,12 +17,12 @@ foreach my $module ( 'Basic', sort { $a cmp $b } Zonemaster::Test->modules ) {
     my $ref  = $full->metadata;
     while ( my ($key, $list) = each %$ref ) {
         for my $tag (map { uc( $module ) . ':' . $_ } sort { $a cmp $b } @$list) {
-	    # printf "%46s -> %s::%s\n", $tag, $module, $key;
-	    my $testmodule = sprintf("%s::%s", $module, $key);
-	    my $testlevel = "$module-TP";
-	    my $testcase = "$key.md";
-	    my $testlink = "$testlevel/$testcase";
-	    printf "| %-30s | [%s](%s) |\n", $tag, $testmodule, $testlink;
+            # printf "%46s -> %s::%s\n", $tag, $module, $key;
+            my $testmodule = sprintf("%s::%s", $module, $key);
+            my $testlevel = "$module-TP";
+            my $testcase = "$key.md";
+            my $testlink = "$testlevel/$testcase";
+            printf "| %-30s | [%s](%s) |\n", $tag, $testmodule, $testlink;
         }
     }
 }
