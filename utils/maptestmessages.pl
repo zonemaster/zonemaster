d9adc1f5552a12c7556f7fd03e3d67b780c535d0
Mattias Päivärinta <mattias.paivarinta@iis.se>
2018-03-02 11:52:15 +0100

    Revert "maptestmessages: Update document layout"
    This reverts commit 5258bcde46ffbe5dfa3c8ec262e775e4b1721a23.

--- utils/maptestmessages.pl
+++ utils/maptestmessages.pl
@@ -8,11 +8,10 @@ use Zonemaster::Engine;
 print "# Mapping test messages to test module\n\n";
 
 # table header
-print "| Log message identifier                         | Implemented test case          |\n";
-print "|:-----------------------------------------------|:-------------------------------|\n";
+print "| Log message identifier        | Implemented test case         |\n";
+print "|:------------------------------|:------------------------------|\n";
 
 # table content
-my %links;
 foreach my $module ( 'Basic', sort { fc $a cmp fc $b } Zonemaster::Engine::Test->modules ) {
     my $full = "Zonemaster::Engine::Test::$module";
     my $ref  = $full->metadata;
@@ -20,17 +19,11 @@ foreach my $module ( 'Basic', sort { fc $a cmp fc $b } Zonemaster::Engine::Test-
         my $list = $ref->{$key};
         for my $tag (map { uc( $module ) . ':' . $_ } sort { $a cmp $b } @$list) {
             # printf "%46s -> %s::%s\n", $tag, $module, $key;
-            my $link = sprintf("[%s::%s]", $module, $key);
-            my $target = sprintf("%s-TP/%s.md", $module, $key);
-            $links{$link} = $target;
-            printf "| %-46s | %-30s |\n", $tag, $link;
+            my $testmodule = sprintf("%s::%s", $module, $key);
+            my $testlevel = "$module-TP";
+            my $testcase = "$key.md";
+            my $testlink = "$testlevel/$testcase";
+            printf "| %-30s | [%s](%s) |\n", $tag, $testmodule, $testlink;
         }
     }
-}
-
-# links footer
-print "\n";
-for my $link (sort keys %links) {
-    my $target = $links{$link};
-    printf "%s: %s\n", $link, $target;
 }
