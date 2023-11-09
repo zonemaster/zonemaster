1cfaf2c5d31abcf56729ce571cf2a2bb9972be50
Mats Dufberg <mats.dufberg@iis.se>
2023-11-09 20:54:23 +0000

    Updates IP addresses for the interface

--- test-zone-data/add-ip.sh
+++ test-zone-data/add-ip.sh
@@ -8,11 +8,23 @@ ip addr add fda1:b2:c3::127:1:0:2/128 dev lo
 
 ip addr add 127.3.0.53/32 dev lo
 
+# xa
+
 ip addr add 127.2.0.11/32 dev lo
 ip addr add fda1:b2:c3::127:2:0:11/128 dev lo
 
 ip addr add 127.2.0.12/32 dev lo
 ip addr add fda1:b2:c3::127:2:0:12/128 dev lo
+
+# xb
+
+ip addr add 127.2.0.13/32 dev lo
+ip addr add fda1:b2:c3::127:2:0:13/128 dev lo
+
+ip addr add 127.2.0.14/32 dev lo
+ip addr add fda1:b2:c3::127:2:0:14/128 dev lo
+
+# Consistency05
 
 ip addr add 127.14.5.21/32 dev lo
 ip addr add fda1:b2:c3::127:14:5:21/128 dev lo
@@ -20,11 +32,45 @@ ip addr add fda1:b2:c3::127:14:5:21/128 dev lo
 ip addr add 127.14.5.22/32 dev lo
 ip addr add fda1:b2:c3::127:14:5:22/128 dev lo
 
+ip addr add 127.14.5.23/32 dev lo
+ip addr add fda1:b2:c3::127:14:5:23/128 dev lo
+
+ip addr add 127.14.5.24/32 dev lo
+ip addr add fda1:b2:c3::127:14:5:24/128 dev lo
+
 ip addr add 127.14.5.31/32 dev lo
 ip addr add fda1:b2:c3::127:14:5:31/128 dev lo
 
 ip addr add 127.14.5.32/32 dev lo
 ip addr add fda1:b2:c3::127:14:5:32/128 dev lo
+
+ip addr add 127.14.5.33/32 dev lo
+ip addr add fda1:b2:c3::127:14:5:33/128 dev lo
+
+ip addr add 127.14.5.34/32 dev lo
+ip addr add fda1:b2:c3::127:14:5:34/128 dev lo
+
+ip addr add 127.14.5.35/32 dev lo
+ip addr add fda1:b2:c3::127:14:5:35/128 dev lo
+
+# Consistency06
+
+ip addr add 127.14.6.21/32 dev lo
+ip addr add fda1:b2:c3::127:14:6:21/128 dev lo
+
+ip addr add 127.14.6.22/32 dev lo
+ip addr add fda1:b2:c3::127:14:6:22/128 dev lo
+
+ip addr add 127.14.6.31/32 dev lo
+ip addr add fda1:b2:c3::127:14:6:31/128 dev lo
+
+ip addr add 127.14.6.32/32 dev lo
+ip addr add fda1:b2:c3::127:14:6:32/128 dev lo
+
+ip addr add 127.14.6.33/32 dev lo
+ip addr add fda1:b2:c3::127:14:6:33/128 dev lo
+
+#
 
 ip addr add 127.15.16.21/32 dev lo
 ip addr add fda1:b2:c3::127:15:16:21/128 dev lo
